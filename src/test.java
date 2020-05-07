bbeb7bacc5779a9a66e7882574fc624d84d041cc
IncheonPark <dnehd8@gmail.com>
2020-05-07 18:53:23 +0900

    init commit JSP_chap18 PM 06:52, 07 May 2020

---
+++ src/test.java
@@ -0,0 +1,12 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "bcdeabcde";
+		
+		int num = str.indexOf("a");
+		System.out.println(num);
+	}
+
+}
